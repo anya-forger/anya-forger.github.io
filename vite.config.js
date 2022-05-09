865ca3d8fc3f62a9d1148cf9bc93bf435989a02f
JacobLinCool <jacoblincool@gmail.com>
2022-05-10 01:51:36 +0800

    feat: add service worker to cache images

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,23 @@
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import Icons from "unplugin-icons/vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue(), Icons({ autoInstall: true, compiler: "vue3" })],
+    plugins: [
+        vue(),
+        Icons({ autoInstall: true, compiler: "vue3" }),
+        VitePWA({
+            workbox: {
+                runtimeCaching: [
+                    {
+                        urlPattern: ({ url }) => url.origin === "https://spy-family.net",
+                        handler: "StaleWhileRevalidate",
+                        options: { cacheName: "external-spy-images" },
+                    },
+                ],
+            },
+        }),
+    ],
 });
