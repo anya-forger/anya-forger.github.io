3d77b2875fff39e0df08d86b0d3b3e877a3fff72
JacobLinCool <jacoblincool@gmail.com>
2022-05-10 01:51:36 +0800

    feat: add service worker to cache images

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from "vue";
+import { registerSW } from "virtual:pwa-register";
 import App from "./App.vue";
 import "./index.css";
 import { router } from "./router";
@@ -7,3 +8,7 @@ const app = createApp(App);
 app.use(router);
 
 app.mount("#app");
+
+if ("serviceWorker" in navigator && !/localhost/.test(window.location)) {
+    registerSW();
+}
